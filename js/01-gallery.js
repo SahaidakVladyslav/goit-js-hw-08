dac90b074511f0e816b509255d7dc8743c18a987
Vladyslav Sahaidak <svg.231@gmail.com>
2023-10-05 13:06:10 +0200

    'mamba'

---
+++ js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(galleryItems);
+
+
+const listEl = document.querySelector('.gallery')
+
+const typset = galleryItems.map(({ preview, original, description }) => {
+    return `
+<li style="margin: 20px;list-style: none;" class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img loading="lazy" class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`}).join('');
+
+listEl.insertAdjacentHTML("beforeend", typset)
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    fadeSpeed: 250,
+    captionSelector: "img",
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
