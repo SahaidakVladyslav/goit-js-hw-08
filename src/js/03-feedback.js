bbf401d6b0185c2195d7aca13a32505d96440df9
Vladyslav Sahaidak <svg.231@gmail.com>
2023-10-06 21:12:09 +0200

    'mambaCommit'

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const formEl = document.querySelector('.feedback-form');
+const emailEl = Array.from(formEl)[0];
+const messageEl = Array.from(formEl)[1];
+const btnEl = Array.from(formEl)[2];
 
+console.log(formEl);
+console.log(emailEl);
+console.log(messageEl);
+console.log(btnEl);
+btnEl.textContent = 'mamba';
+const data = {
+    email: '',
+    message: ''
+};
+// localStorage.setItem(key, value)
+emailEl.addEventListener('input', (event) => {
+    data.email = event.currentTarget.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+    console.log(event.currentTarget.value);
+})
+
+messageEl.addEventListener('input', (event) => {
+    data.message = event.currentTarget.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+    console.log(event.currentTarget.value);
+});
+btnEl.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(data);
+    formEl.reset();
+});
+
+// throttle(function (data) {
+//     player.getCurrentTime().then(function (seconds) {
+//         console.log(seconds);
+//         localStorage.setItem("videoplayer-current-time", seconds);
+//     })
+// }, 2000)
