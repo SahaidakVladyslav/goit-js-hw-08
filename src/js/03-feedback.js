ad2bc1a81afab97739615e93fe3a100a79cfac13
Vladyslav Sahaidak <svg.231@gmail.com>
2023-10-07 02:49:56 +0200

    'lastTask'

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,37 @@
+import throttle from 'lodash.throttle';
+
 const formEl = document.querySelector('.feedback-form');
 const emailEl = Array.from(formEl)[0];
 const messageEl = Array.from(formEl)[1];
 const btnEl = Array.from(formEl)[2];
 
-console.log(formEl);
-console.log(emailEl);
-console.log(messageEl);
-console.log(btnEl);
-btnEl.textContent = 'mamba';
+
 const data = {
     email: '',
     message: ''
 };
-// localStorage.setItem(key, value)
-emailEl.addEventListener('input', (event) => {
-    data.email = event.currentTarget.value;
+
+
+// const emailInput = (event) => {
+//     data.email = emailEl.value;
+//     localStorage.setItem("feedback-form-state", JSON.stringify(data));
+//     console.log(emailEl.value);
+// }
+
+
+
+emailEl.addEventListener('input', throttle(function emailInput(event) {
+    data.email = emailEl.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(data));
-    console.log(event.currentTarget.value);
-})
+    console.log(emailEl.value);
+}, 1000))
+
+
 
 messageEl.addEventListener('input', (event) => {
-    data.message = event.currentTarget.value;
+    data.message = messageEl.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(data));
-    console.log(event.currentTarget.value);
+    console.log(messageEl.value);
 });
 btnEl.addEventListener('click', (event) => {
     event.preventDefault();
@@ -30,9 +39,3 @@ btnEl.addEventListener('click', (event) => {
     formEl.reset();
 });
 
-// throttle(function (data) {
-//     player.getCurrentTime().then(function (seconds) {
-//         console.log(seconds);
-//         localStorage.setItem("videoplayer-current-time", seconds);
-//     })
-// }, 2000)
