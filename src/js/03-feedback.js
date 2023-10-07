6cfd370b0dadf688dca6db8840401139f688f1b5
Vladyslav Sahaidak <svg.231@gmail.com>
2023-10-07 22:40:51 +0200

    'nearlyDone'

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,38 +4,53 @@ const formEl = document.querySelector('.feedback-form');
 const emailEl = Array.from(formEl)[0];
 const messageEl = Array.from(formEl)[1];
 const btnEl = Array.from(formEl)[2];
-
-
 const data = {
     email: '',
     message: ''
 };
-
-
-// const emailInput = (event) => {
-//     data.email = emailEl.value;
-//     localStorage.setItem("feedback-form-state", JSON.stringify(data));
-//     console.log(emailEl.value);
-// }
-
+function saveDataStorage() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}
+const dataStorage = localStorage.getItem("feedback-form-state");
+const dataEmailValue = () => {
+    if (dataStorage) {
+        return JSON.parse(dataStorage).email
+    }
+};
+const dataMessageValue = () => {
+    if (dataStorage) {
+        return JSON.parse(dataStorage).message;
+    }
+}
 
 
 emailEl.addEventListener('input', throttle(function emailInput(event) {
     data.email = emailEl.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(data));
-    console.log(emailEl.value);
-}, 1000))
+    saveDataStorage();
+}, 500));
 
-
-
-messageEl.addEventListener('input', (event) => {
+messageEl.addEventListener('input', throttle(function (event) {
     data.message = messageEl.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(data));
-    console.log(messageEl.value);
-});
+    saveDataStorage();
+}, 500));
+
 btnEl.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(data);
     formEl.reset();
 });
 
+if (localStorage.getItem("feedback-form-state")) {
+    console.log('yes, i m stiil alive')
+    emailEl.value = dataEmailValue();
+    messageEl.value = dataMessageValue();
+} else {
+    false
+};
+
+
+document.querySelector('body').insertAdjacentHTML('afterend', `<button  style="margin: 30px;background-color: tomato;color:white;" class="reset">reset localStorage data</button>`)
+const resetEl = document.querySelector('.reset');
+resetEl.addEventListener('click', (event) => {
+    localStorage.removeItem("feedback-form-state")
+});
