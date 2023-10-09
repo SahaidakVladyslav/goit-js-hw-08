4d3b5bc6a557d7a0df51807a711bed63897ef337
Vladyslav Sahaidak <svg.231@gmail.com>
2023-10-09 23:43:30 +0200

    'inThisTimeWorkIsDone'

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,26 +2,14 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframeEl = document.querySelector('#vimeo-player');
 const player = new Player(iframeEl);
-
+const keyLocalStorage = "videoplayer-current-time";
 player.setQuality('240p');
 
-player.on('timeupdate', throttle(function (data) {
-    player.getCurrentTime().then(function (seconds) {
-        console.log(seconds);
-        localStorage.setItem("videoplayer-current-time", seconds);
-    })
-}, 2000));
+player.on('timeupdate', throttle(function ({ seconds }) {
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (seconds) {
-    seconds = localStorage.getItem('videoplayer-current-time')
-}).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+    localStorage.setItem(keyLocalStorage, seconds);
+
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem(keyLocalStorage) || 0)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,57 +4,56 @@ const formEl = document.querySelector('.feedback-form');
 const emailEl = Array.from(formEl)[0];
 const messageEl = Array.from(formEl)[1];
 const btnEl = Array.from(formEl)[2];
+const keyLocalStorage = "feedback-form-state";
 const data = {
     email: '',
     message: ''
 };
 function saveDataStorage() {
-    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+    localStorage.setItem(keyLocalStorage, JSON.stringify(data));
 }
-const dataStorage = localStorage.getItem("feedback-form-state");
+const dataStorage = () => { return localStorage.getItem(keyLocalStorage) };
 const dataEmailValue = () => {
-    if (dataStorage) {
-        return JSON.parse(dataStorage).email
+    if (dataStorage()) {
+        return JSON.parse(dataStorage()).email
     }
 };
 const dataMessageValue = () => {
-    if (dataStorage) {
-        return JSON.parse(dataStorage).message;
+    if (dataStorage()) {
+        return JSON.parse(dataStorage()).message;
     }
 }
 
-
-emailEl.addEventListener('input', throttle(function emailInput(event) {
+if (dataStorage()) {
+    emailEl.value = dataEmailValue();
     data.email = emailEl.value;
-    saveDataStorage();
-}, 500));
-
-messageEl.addEventListener('input', throttle(function (event) {
+    messageEl.value = dataMessageValue();
     data.message = messageEl.value;
+    console.log('В російських школах відключать дзвінки, щоб діти не чекали «пєрємєн»')
+}
+
+
+
+formEl.addEventListener('input', throttle(function emailInput(event) {
+    let eventName = event.target.name;
+    if (eventName === 'email') {
+        data.email = event.target.value;
+    }
+    if (eventName === 'message') {
+        data.message = event.target.value;
+    }
     saveDataStorage();
 }, 500));
 
-btnEl.addEventListener('click', (event) => {
+
+formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     if (emailEl.value !== "" && messageEl.value !== "") {
-        console.log(data);
+        console.log(dataStorage())
+        localStorage.removeItem("feedback-form-state")
         formEl.reset();
     } else {
         alert("Please fill in all fields")
     }
 });
 
-if (localStorage.getItem("feedback-form-state")) {
-    console.log('yes, i m stiil alive')
-    emailEl.value = dataEmailValue();
-    messageEl.value = dataMessageValue();
-} else {
-    false
-};
-
-
-document.querySelector('body').insertAdjacentHTML('afterend', `<button  style="margin: 30px;background-color: tomato;color:white;" class="reset">reset localStorage data</button>`)
-const resetEl = document.querySelector('.reset');
-resetEl.addEventListener('click', (event) => {
-    localStorage.removeItem("feedback-form-state")
-});
